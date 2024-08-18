06441a7a2dc36965d9eabb9a89c2c16dd52dac08
lorenzo <lolitobuffo@gmail.com>
2024-08-18 19:13:10 -0300

    PRIMERA MODIFICACION

---
+++ scenes/GameOver.js
@@ -0,0 +1,12 @@
+export default class GameOver extends Phaser.Scene {
+    constructor() {
+      super("GameOver");
+      }
+    
+
+    create(){
+        
+       }
+    }
+
+    
--- scenes/HelloWorldScene.js
+++ scenes/HelloWorldScene.js
@@ -7,40 +7,76 @@ export default class HelloWorldScene extends Phaser.Scene {
     super("hello-world");
   }
 
-  init() {
-    // this is called before the scene is created
-    // init variables
-    // take data passed from other scenes
-    // data object param {}
-  }
+  create(){
+    //crear la paleta rectangular
+    this.paddle = this.add.rectangle(400, 500, 100, 20,0x6666ff);
 
-  preload() {
-    // load assets
-    this.load.image("sky", "./assets/space3.png");
-    this.load.image("logo", "./assets/phaser3-logo.png");
-    this.load.image("red", "./assets/particles/red.png");
-  }
+    //crear la pelota
+    this.ball = this.add.circle(310, 300, 10, 0xff6666);
 
-  create() {
-    // create game objects
-    this.add.image(400, 300, "sky");
+    //agregar obstaculo
+    this.obstacle = this.add.rectangle(400, 250, 100, 50, 0x66ff66);
 
-    const logo = this.physics.add.image(400, 100, "logo");
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
+    //agregar fisicas a los objetos
+    this.physics.add.existing(this.paddle);
+    this.physics.add.existing(this.ball);
+    this.physics.add.existing(this.obstacle);
 
-    // emmit particles from logo
-    const emitter = this.add.particles(0, 0, "red", {
-      speed: 100,
-      scale: { start: 1, end: 0 },
-      blendMode: "ADD",
+    //FISICAS DE LA PALETA
+    //hacer la paleta inamovible
+    this.paddle.body.setImmovable(true);
+    //agregar choque de la paleta con los limites de la pantalla
+    this.paddle.body.setCollideWorldBounds(true);
+    //hacer que la paleta no se vea afectada por la gravedad
+    this.paddle.body.setAllowGravity(false);
+
+    //agregar cursor
+     // Agregar el puntero del mouse
+     this.mousePointer = this.input.activePointer;
+
+    //FISICAS DE LA PELOTA
+    //colision con los limites
+    this.ball.body.setCollideWorldBounds(true);
+    //rebote de la pelota
+    this.ball.body.setBounce(1, 1);
+    //velocidad de la pelota
+    this.ball.body.setVelocity(200, 200);
+
+    //FISICAS DE LOS OBSTACULOS
+    //hacer al obstaculo inamovible
+    this.obstacle.body.setImmovable(true);
+    //hacer que el obstaculo no se vea afectado por la gravedad
+    this.obstacle.body.setAllowGravity(false);
+
+    //COLISIONES
+    //colision de la pelota con la paleta
+    this.physics.add.collider(this.paddle, this.ball, null, null, this);
+
+    //colision de la pelota con el obstaculo
+    this.ball.body.onWorldBounds = true;
+    this.physics.add.collider(
+      this.obstacle,
+      this.ball,
+      this.handleCollision,
+      null,
+      this
+    );
+     //colision de la pelota con el limite inferior
+     this.physics.world.on("worldbounds", (body, up, down, left, right) => {
+      if (down) {
+        console.log("hit bottom");
+        this.scene.start("GameOver");
+      }
     });
-
-    emitter.startFollow(logo);
   }
 
-  update() {
-    // update game objects
+  update(){
+   // Mueve la paleta con el cursor del mouse
+   this.paddle.x = this.mousePointer.x;
   }
+  handleCollision = (obstacle, ball) => {
+    console.log("collision");
+    obstacle.destroy();
+  };
 }
+
